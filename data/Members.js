11adbd1f517367be34b6bb486685fb4a47ca356f
davieu <davisumana@gmail.com>
2019-06-23 13:50:54 -0400

    made get endpoints and a json file

---
+++ data/Members.js
@@ -0,0 +1,32 @@
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active',
+    role: 'teacher'
+  },
+  {
+    id: 2,
+    name: 'Bob Williams',
+    email: 'bwilliams@gmail.com',
+    status: 'active',
+    role: 'cleaner'
+  },
+  {
+    id: 3,
+    name: 'Jim Bob',
+    email: 'jimbob@gmail.com',
+    status: 'inactive',
+    role: 'plumber'
+  },
+  {
+    id: 4,
+    name: 'Ben Chu',
+    email: 'chub@gmail.com',
+    status: 'active',
+    role: 'coder'
+  }
+]
+
+module.exports = members
--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 const express = require('express');
-
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
+const moment = require('moment')
+
+const members = require('./data/Members')
+const logger = require('./middleware/middleware.js')
+
+
+let membersJSON = []
+
+// INIT MIDDLEWARE
+// app.use(logger);
+
+fs.readFile(path.join(__dirname, '/data', 'members.json'), 'utf8', (err, data) => {
+  if (err) throw err
+  membersJSON = JSON.parse(data)
+});
+
+// GETS ALL MEMBERS FROM JSON
+app.get('/api/members', (req, res) => {
+  res.json(membersJSON);
+})
+
+// GET ALL MEMBER FROM JS OBJECT
+app.get('/api/membersjs', (req, res) => {
+  res.json(members);
+})
+
+// GET SINGLE MEMBER
+app.get('/api/member/:id', (req, res) => {
+  // res.json(membersJSON[req.params.id])
+  res.json(membersJSON.filter(member => member.id === parseInt(req.params.id)))
+})
+
+// GET ALL ACTIVE MEMBERS
+app.get('/api/members-active', (req, res) => {
+  res.json(membersJSON.filter(member => member.status == 'active'))
+}) 
+
+//GET ALL INACTIVE MEMBERS
+app.get('/api/members-inactive', (req, res) => {
+  res.json(membersJSON.filter(member => member.status == 'inactive'))
+})
+
+// SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen();
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+// send the file or page on '/'
+// app.get('/', (req, res) => {
+//   res.sendfile(path.join(__dirname, 'public', 'index.html'));
+// });
